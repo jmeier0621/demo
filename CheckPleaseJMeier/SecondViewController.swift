fdea63572758bd32d4827a6b332c85bf11f18f7e
student <s518807@ch140014.nwmissouri.edu>
2017-09-25 12:02:40 -0500

    Initial Commit

---
+++ CheckPleaseJMeier/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CheckPleaseJMeier
+//
+//  Created by Meier,Jesse A on 9/25/17.
+//  Copyright © 2017 Jesse Meier. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
